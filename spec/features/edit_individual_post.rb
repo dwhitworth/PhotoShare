5aa9c2e5c4c79b0e5a4977a91082a4be20f41498
Donovan Whitworth <donovan.whitworth@gmail.com>
2019-08-25 15:48:38 +1000

    change copy on edit post

--- spec/features/edit_individual_post.rb
+++ spec/features/edit_individual_post.rb
@@ -9,10 +9,10 @@ feature 'Editing posts' do
     click_link 'Edit Post'
   end
   scenario 'Can edit a post' do
-    fill_in 'Caption', with: "Oh god, you weren't meant to see this picture!"
+    fill_in 'Caption', with: "Post to delete later"
     click_button 'Update Post'
 
-    expect(page).to have_content("Post updated hombre")
-    expect(page).to have_content("Oh god, you weren't meant to see this picture!")
+    expect(page).to have_content("Post updated")
+    expect(page).to have_content("Post to delete later")
   end
 end
